ac313fb5043b49eba56c1ca70b19c00deb086fde
Anuja Negi <anujanegi1@gmail.com>
2021-04-30 18:03:43 +0200

    added pipelining

--- generate_bem.py
+++ generate_bem.py
@@ -1,5 +1,6 @@
 import os
 from mne.bem import make_watershed_bem
+from joblib import Parallel, delayed
 
 # the paths to Freesurfer reconstructions
 DATA_PATH = '/datasabzi/anuja/freesurfer'
@@ -9,10 +10,12 @@ def generate_bem(subject, subject_dir):
     """
     Creates BEM surfaces using the FreeSurfer watershed algorithm for the given subject.
     """
-    make_watershed_bem(subject, subject_dir)
+    make_watershed_bem(subject, subject_dir, overwrite=True)
 
 if __name__ == "__main__":
+    subjects = []
     for subject in os.listdir(SUBJECT_DIR):
         if subject.startswith("CC"):
-            generate_bem(subject, SUBJECT_DIR)
-            
+            subjects.append(subject)
+    
+    Parallel(n_jobs=25)(delayed(generate_bem)(subject, SUBJECT_DIR) for subject in subjects)
